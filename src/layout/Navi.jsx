4281171283c642be87335980457f6a51e61979f9
Emre Aka <aka548548@gmail.com>
2021-06-06 21:55:31 +0300

    Navigation bar added.

---
+++ src/layout/Navi.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Button, Dropdown, Menu } from 'semantic-ui-react'
+
+export default function Navi() {
+    return (
+        <div>
+            <Menu size='mini'>
+                <Menu.Item
+                    name='home'
+                />
+                <Menu.Item
+                    name='messages'
+                />
+
+                <Menu.Menu position='right'>
+                    <Dropdown item text='Language'>
+                        <Dropdown.Menu>
+                            <Dropdown.Item>English</Dropdown.Item>
+                            <Dropdown.Item>Russian</Dropdown.Item>
+                            <Dropdown.Item>Spanish</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+                    <Menu.Item>
+                        <Button primary>Sign Up</Button>
+                    </Menu.Item>
+                </Menu.Menu>
+            </Menu>
+        </div>
+    )
+}
