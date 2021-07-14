67ca4f435adbc1a3a230bb727a96b55a0502c73f
Emre Aka <aka548548@gmail.com>
2021-07-15 01:46:45 +0300

    Favorite Jobs
    Pagination added.

--- src/pages/JobAdvertisementList.jsx
+++ src/pages/JobAdvertisementList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import {Icon, Header, Pagination} from 'semantic-ui-react'
+import {Icon, Header, Pagination, Menu, Dropdown, Button} from 'semantic-ui-react'
 import JobAdvertisementService from '../services/jobAdvertisementService'
 
 import JobAdvertisementsItem from "../components/JobAdvertisementsItem";
@@ -31,12 +31,27 @@ export default function JobAdvertisement() {
     const totalPosts = JobAdvertisements.length
     const totalPages = totalPosts / JobAdvertisementsPerPage
 
+    const itemPerPage = [1,2,3,40,50,60,70,80,90,100]
+
     return (
         <div>
+            <Menu>
+                <Dropdown item text='Job Advertisement per page' >
+                    <Dropdown.Menu>
+                        {itemPerPage.map(item => {
+                            return <Dropdown.Item key={item} onClick={(event, data) => {setJobAdvertisementsPerPage(item)}}>
+                                {item}
+                            </Dropdown.Item>
+                        })}
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Menu>
+
             <Header as="h2">
                 <Icon name="list alternate outline" />
                 <Header.Content>Job List</Header.Content>
             </Header>
+
             <JobAdvertisementsItem jobAdvertisements={currentJobAdvertisements} loading={Loading} />
 
             <Pagination defaultActivePage={1} totalPages={totalPages} onPageChange={(event, data) => {setCurrentPage(data.activePage)}} />
