fd625def394b8e7f9c8f4af13e08ef2aa1737bb9
Emre Aka <aka548548@gmail.com>
2021-07-29 13:53:33 +0300

    Updated
    I changed some pages' designs.

--- src/pages/FavoriteJobAdvertisements.jsx
+++ src/pages/FavoriteJobAdvertisements.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import {Button, Header, Icon, Item} from "semantic-ui-react";
+import {Button, Card, Header, Icon, Item} from "semantic-ui-react";
 import {Link} from "react-router-dom";
 import {toast} from "react-toastify";
 import { useDispatch, useSelector } from 'react-redux';
@@ -19,41 +19,43 @@ export default function FavoriteJobAdvertisement() {
 
     return (
         <div>
-            <Header as="h2">
-                <Icon name="list alternate outline"/>
-                <Header.Content>Favorite Jobs</Header.Content>
-            </Header>
-            <Item.Group divided>
-                {
-                    favjobs.map(favoritejob => (
-                        <Item key={favoritejob.id}>
-                            <Item.Image size='small'
-                                        src='https://res.cloudinary.com/emreaka/image/upload/v1624304366/job_o67inx.jpg'/>
-                            <Item.Content>
-                                <Item.Header>{favoritejob.job.employer.companyName}</Item.Header>
-                                <Item.Meta>
-                                    <span className='price'>{favoritejob.job.maxSalary} TL</span>
-                                    <span className='stay'>{favoritejob.job.city.cityName}</span>
-                                </Item.Meta>
-                                <Item.Description>{favoritejob.job.description}</Item.Description>
-                                <Link to={`/jobs/${favoritejob.job.id}`}>
-                                    <Button animated color='grey'>
-                                        <Button.Content visible>Job's Details</Button.Content>
-                                        <Button.Content hidden>
-                                            <Icon name='arrow right'/>
-                                        </Button.Content>
-                                    </Button>
+            <Card fluid>
+                <Card.Content header = 'Favorite Jobs'/>
+                <Card.Content>
+                    <Item.Group divided>
+                        {
+                            favjobs.map(favoritejob => (
+                                <Item key={favoritejob.id}>
+                                    <Item.Image size='small'
+                                                src='https://res.cloudinary.com/emreaka/image/upload/v1624304366/job_o67inx.jpg'/>
+                                    <Item.Content>
+                                        <Item.Header>{favoritejob.job.employer.companyName}</Item.Header>
+                                        <Item.Meta>
+                                            <span className='price'>{favoritejob.job.maxSalary} TL</span>
+                                            <span className='stay'>{favoritejob.job.city.cityName}</span>
+                                        </Item.Meta>
+                                        <Item.Description>{favoritejob.job.description}</Item.Description>
+                                        <Link to={`/jobs/${favoritejob.job.id}`}>
+                                            <Button animated color='grey'>
+                                                <Button.Content visible>Job's Details</Button.Content>
+                                                <Button.Content hidden>
+                                                    <Icon name='arrow right'/>
+                                                </Button.Content>
+                                            </Button>
 
-                                </Link>
-                                <Button animated = 'fade' color = 'red' onClick={() => handleRemoveFromFavoriteDb(favoritejob)}>
-                                    <Button.Content visible>Remove From Favorites</Button.Content>
-                                    <Button.Content hidden><Icon name = 'trash'/></Button.Content>
-                                </Button>
-                            </Item.Content>
-                        </Item>
-                    ))
-                }
-            </Item.Group>
+                                        </Link>
+                                        <Button animated = 'fade' color = 'red' onClick={() => handleRemoveFromFavoriteDb(favoritejob)}>
+                                            <Button.Content visible>Remove From Favorites</Button.Content>
+                                            <Button.Content hidden><Icon name = 'trash'/></Button.Content>
+                                        </Button>
+                                    </Item.Content>
+                                </Item>
+                            ))
+                        }
+                    </Item.Group>
+                </Card.Content>
+            </Card>
+
 
             {/* {JSON.stringify(favjobs)} */}
 
